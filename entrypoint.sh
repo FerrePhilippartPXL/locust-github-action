cb81a5b7eb198bbe9c6f1cf95279090e5fce8de6
FerrePhilippartPXL <ferre.philippart@student.pxl.be>
2024-03-15 12:49:31 +0100

    added .csv on master branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,4 +17,4 @@ else
 fi
 
 # Run locust script
-locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
+locust -f $file --csv LoadTest --headless -t10m -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
